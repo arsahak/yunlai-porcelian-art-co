352e777ad6a0d119cb66d9ff3938fc03de8209f8
arsahak <arsahak99@gmail.com>
2026-04-09 00:05:44 +0600

    update sitemap and google search console

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Topbar from "@/components/layout/Topbar";
 import LocaleProvider from "@/components/shared/LocaleProvider";
 import { Inter } from "next/font/google";
 import localFont from "next/font/local";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
@@ -29,6 +30,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-SS0V2JZRZS"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-SS0V2JZRZS');
+          `}
+        </Script>
+      </head>
       <body className={`${inter.variable} ${kefirMedium.variable} antialiased`}>
         <LocaleProvider>
           <Topbar />
